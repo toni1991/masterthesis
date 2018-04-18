ffec99fdb06b2e53d120017a35a23f7650634fb5
Antonio Grieco <antonio.grieco@student.uni-augsburg.de>
2018-04-18 17:02:59 +0200

    dasd

---
+++ Code/newCobol.cbl
@@ -0,0 +1,17 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HELLO_USER.                          
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+           01 USERNAME PIC X(20) VALUE SPACES.      
+
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            DISPLAY "Your name: " WITH NO ADVANCING.    
+            ACCEPT USERNAME.                            
+            IF USERNAME EQUALS SPACES                   
+                MOVE "world" TO USERNAME                
+            END-IF.                                     
+            DISPLAY "Hello " USERNAME.                  
+            STOP RUN.
+       END PROGRAM HELLO_USER.                          
