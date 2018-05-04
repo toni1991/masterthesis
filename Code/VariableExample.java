8f0cb84cf27c111bfa15c876494de62a1420bf1f
Antonio Grieco <antonio.grieco@student.uni-augsburg.de>
2018-05-04 18:30:47 +0200

    Added various.

--- Code/VariableExample.java
+++ Code/VariableExample.java
@@ -1,9 +1,11 @@
 public class VariableExample {
 
+    final int CONSTANT_VARIABLE = 1907;
     int primitiveClassVariable = 0;
     VariableExample complexClassVariable = null;
 
     public static void main(String[] args) {
+        // CONSTANT_VARIABLE = 1860 -> Fehler
         int primitiveLocalVariable = 1;
         VariableExample complexLocalVariable = new VariableExample();
     }
