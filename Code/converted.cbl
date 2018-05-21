eb1937fb8811f6f18a5d51695586b783600c7200
Antonio Grieco <antonio.grieco@student.uni-augsburg.de>
2018-05-21 20:47:42 +0200

    Updated on PC.

--- Code/converted.cbl
+++ Code/converted.cbl
@@ -1,6 +1,6 @@
-(44)  01  W-DATE-FORM.
-(45)      05 W-JJ                PIC XX.
-(46)      05 FILLER              PIC X            VALUE '.'.
-(47)      05 W-MM                PIC XX.
-(48)      05 FILLER              PIC X            VALUE '.'.
-(49)      05 W-AA                PIC XX.
+       01  W-DATE-FORM.
+           05 W-JJ                PIC XX.
+           05 FILLER              PIC X            VALUE '.'.
+           05 W-MM                PIC XX.
+           05 FILLER              PIC X            VALUE '.'.
+           05 W-AA                PIC XX.
